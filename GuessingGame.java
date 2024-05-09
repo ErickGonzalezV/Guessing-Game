ea81ba980a687b630e62a6116d8d907b4a3e3cf3
Erick Gonzalez <134249763+ErickGonzalezV@users.noreply.github.com>
2024-05-09 16:29:31 -0600

    Create GuessingGame.java

---
+++ GuessingGame.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGame {
+public static void main(String[] args) {
+    new Random().nextInt(100);
+    Scanner teclado = new Scanner(System.in);
+    int adivinanza = 0;
+    int intentos = 5;
+
+    while (adivinanza != ){
+
+}
+}
