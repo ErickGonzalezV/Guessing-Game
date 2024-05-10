9d16f0be36df08e647b5219827dba39c0e75ce4e
Erick Gonzalez <134249763+ErickGonzalezV@users.noreply.github.com>
2024-05-09 20:10:17 -0600

    Update GuessingGame.java
    Completed

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,14 +1,27 @@
-import java.util.Random;
-import java.util.Scanner;
+public class JuegoDeAdivinacion {
+    public static void main(String[] args) {
 
-public class GuessingGame {
-public static void main(String[] args) {
-    new Random().nextInt(100);
-    Scanner teclado = new Scanner(System.in);
-    int adivinanza = 0;
-    int intentos = 5;
+                Scanner lector = new Scanner(System.in);
+                int numeroGenerado = new Random().nextInt(100); // genera un número aleatorio entre 0 y 100
+                int intentos = 0;
 
-    while (adivinanza != ){
+                while (intentos < 5) {
+                    System.out.print("Introduzca un número entre 0 e 100: ");
+                    int numeroDelUsuario = lector.nextInt();
+                    intentos++;
 
-}
-}
+                    if (numeroDelUsuario == numeroGenerado) {
+                        System.out.println("Felicidades, adivinaste el número en " + intentos + " intentos!");
+                        break; // interrumpe el loop while
+                    } else if (numeroDelUsuario < numeroGenerado) {
+                        System.out.println("El número que escribiste es menor al número generado.");
+                    } else {
+                        System.out.println("El número que escribiste es mayor al número generado.");
+                    }
+                }
+
+                if (intentos == 5) {
+                    System.out.println("Lo siento, no conseguiste adivinar el número en 5 intentos. El número era " + numeroGenerado);
+                }
+            }
+        }
